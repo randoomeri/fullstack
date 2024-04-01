ad57c72e289270099a2f1ed5b7e9bd532c57c05d
toka_kayttaja <105450573+randoomeri@users.noreply.github.com>
2024-04-01 15:55:35 +0300

    Osat 1.1 ja 1.2

---
+++ osa1/kurssitiedot/src/App.jsx
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.name} {props.number}
+      </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.name1} number={props.number1} />
+      <Part name={props.name2} number={props.number2} />
+      <Part name={props.name2} number={props.number2} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <>
+      <p>Number of exercises {props.amount}</p>
+    </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content name1={part1} number1={exercises1} name2={part2} number2={exercises2}
+       name3={part3} number3={exercises3} />
+      <Total amount={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
