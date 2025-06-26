4774008b6591735e58359b6e19e9ee968e2d422c
toka_kayttaja <105450573+randoomeri@users.noreply.github.com>
2025-06-26 16:05:29 +0300

    unicafe step6

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -22,19 +22,26 @@ const Statistics = ({good, bad, neutral}) => {
                     fontWeight: '600'}}>
           Statistics
         </h2>
-        <StatisticLine text="Good" value={good}/>
-        <StatisticLine text="Neutral" value={neutral}/>
-        <StatisticLine text="Bad" value={bad}/>
-        <StatisticLine text="All" value={all}/>
-        <StatisticLine text="Average" value={(good-bad)/all}/>
-        <StatisticLine text="Positive" value={positive + " %"}/>
+        <table>
+          <tbody>
+            <StatisticLine text="Good" value={good}/>
+            <StatisticLine text="Neutral" value={neutral}/>
+            <StatisticLine text="Bad" value={bad}/>
+            <StatisticLine text="All" value={all}/>
+            <StatisticLine text="Average" value={(good-bad)/all}/>
+            <StatisticLine text="Positive" value={positive + " %"}/>
+          </tbody>
+        </table>
     </div>
   )
 }
 
 const StatisticLine = ({text, value}) => {
   return(
-    <p>{text} {value}</p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
