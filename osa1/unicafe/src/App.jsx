6693c4dd38bc0c1ffb566d90430379eb68e4bb25
toka_kayttaja <105450573+randoomeri@users.noreply.github.com>
2025-06-26 15:11:46 +0300

    unicafe step3

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -1,40 +1,43 @@
 import { useState } from 'react'
 
+const Statistics = ({good, bad, neutral}) => {
+  const all = good+bad+neutral
+  let all0 = all
+  if (all == 0) {
+    all0 = 1
+  }
+  return(
+    <div>
+      <h2 style={{fontSize: 26,
+                    fontWeight: '600'}}>
+          Statistics
+        </h2>
+        <p>Good {good}</p>
+        <p>Neutral {neutral}</p>
+        <p>Bad {bad}</p>
+        <p>All {all}</p>
+        <p>Average {(good-bad)/all0}</p>
+        <p>Positive {good/all0*100} %</p>
+    </div>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPos] = useState(0)
 
   const handleGood = () => {
-    const updatedGood = good + 1
-    const updatedAll = all + 1
-    const updatedPositive = updatedGood/updatedAll*100
-    setGood(updatedGood)
-    setAll(updatedAll)
-    setAverage((updatedGood - bad)/updatedAll)
-    setPos(updatedPositive)
+    setGood(good + 1)
   }
 
   const handleBad = () => {
-    const updatedBad = bad + 1
-    setBad(updatedBad)
-    const updatedAll = all + 1
-    setAll(updatedAll)
-    setAverage((good - updatedBad)/updatedAll)
-    setPos(good/updatedAll*100)
+    setBad(bad + 1)
   }
 
   const handleNeutral = () => {
-    const updatedNeutral = neutral + 1
-    const updatedAll = all + 1
-    setNeutral(updatedNeutral)
-    setAll(updatedAll)
-    setAverage((good - bad)/updatedAll)
-    setPos(good/updatedAll*100)
+    setNeutral(neutral + 1)
   }
 
   return (
@@ -46,16 +49,7 @@ const App = () => {
       <button onClick={handleGood}>Good</button>
       <button onClick={handleNeutral}>Neutral</button>
       <button onClick={handleBad}>Bad</button>
-      <h2 style={{fontSize: 26,
-                  fontWeight: '600'}}>
-        Statistics
-      </h2>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {all}</p>
-      <p>Average {average}</p>
-      <p>Positive {positive} %</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   )
 }
