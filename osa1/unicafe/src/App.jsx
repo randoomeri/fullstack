d3c6c251bad87324ffda52c16cb9bd5379f30e31
toka_kayttaja <105450573+randoomeri@users.noreply.github.com>
2025-06-26 14:36:47 +0300

    unicafe step2

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -5,17 +5,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPos] = useState(0)
 
   const handleGood = () => {
-    setGood(good + 1)
+    const updatedGood = good + 1
+    const updatedAll = all + 1
+    const updatedPositive = updatedGood/updatedAll*100
+    setGood(updatedGood)
+    setAll(updatedAll)
+    setAverage((updatedGood - bad)/updatedAll)
+    setPos(updatedPositive)
   }
 
   const handleBad = () => {
-    setBad(bad + 1)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    const updatedAll = all + 1
+    setAll(updatedAll)
+    setAverage((good - updatedBad)/updatedAll)
+    setPos(good/updatedAll*100)
   }
 
   const handleNeutral = () => {
-    setNeutral(neutral + 1)
+    const updatedNeutral = neutral + 1
+    const updatedAll = all + 1
+    setNeutral(updatedNeutral)
+    setAll(updatedAll)
+    setPos(good/updatedAll*100)
   }
 
   return (
@@ -34,6 +52,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
     </div>
   )
 }
